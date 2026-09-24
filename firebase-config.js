1f5a8991f4385a261036d437eb89f3ab2f3bb2be
ryansambath-ux <ryansambath@gmail.com>
2026-09-24 15:17:13 +1000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Verdant Ventures Firebase web configuration
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLt_hpMacVEdkvBLSn4bGq4OMscTSey4A",
+  authDomain: "verdant-ventures-62ab6.firebaseapp.com",
+  projectId: "verdant-ventures-62ab6",
+  storageBucket: "verdant-ventures-62ab6.firebasestorage.app",
+  messagingSenderId: "305122502323",
+  appId: "1:305122502323:web:fd46608c5b6152d05b72c6"
+};
